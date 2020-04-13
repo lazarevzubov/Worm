2b2527796dcec3d2cbacc757cf118a721ec2b752
Nikita Lazarev-Zubov <nikita.lazarev-zubov@dreambroker.fi>
2020-04-13 14:24:21 +0300

    Initial Commit

---
+++ Worm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Worm
+//
+//  Created by Nikita Lazarev-Zubov on 13.4.2020.
+//  Copyright © 2020 Nikita Lazarev-Zubov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
