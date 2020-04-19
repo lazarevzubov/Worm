a044b6e0f9a9d30325647ab0a91ad5b41f2042cd
Nikita Lazarev-Zubov <nikita.lazarev-zubov@dreambroker.fi>
2020-04-19 19:49:04 +0300

    Refactor to coordinators

--- Worm/Sources/Main/SceneDelegate.swift
+++ Worm/Sources/Main/SceneDelegate.swift
@@ -6,38 +6,54 @@
 //  Copyright © 2020 Nikita Lazarev-Zubov. All rights reserved.
 //
 
-import UIKit
+import Coordinator
 import SwiftUI
+import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // MARK: - Properties
 
-    var window: UIWindow?
+    var window: UIWindow? {
+        didSet {
+            if let window = window {
+                coordinator = MainCoordinator(window: window)
+            }
+        }
+    }
+
+    // MARK: Private properties
+
+    private var coordinator: Coordinator? {
+        didSet { coordinator?.start() }
+    }
 
     // MARK: - Methods
 
     // MARK: UIWindowSceneDelegate protocol methods
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // FIXME: SRP.
-
-        guard let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
-            fatalError("Impossible state: shared UIApplication delegate is not an AppDelegate.")
-        }
-
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        // guard let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
+            // fatalError("Impossible state: shared UIApplication delegate is not an AppDelegate.")
+        // }
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        // let contentView = ContentView().environment(\.managedObjectContext, context)
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
-            window.makeKeyAndVisible()
         }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        saveContext()
+    }
+
+    // MARK: Private properties
+
+    private func saveContext() {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
---
+++ Worm/Sources/Navigation/MainCoordinator.swift
@@ -0,0 +1,41 @@
+//
+//  MainCoordinator.swift
+//  Worm
+//
+//  Created by Nikita Lazarev-Zubov on 19.4.2020.
+//  Copyright © 2020 Nikita Lazarev-Zubov. All rights reserved.
+//
+
+import Coordinator
+import SwiftUI
+import UIKit
+
+// TODO: HeaderDoc.
+
+final class MainCoordinator: Coordinator {
+
+    // MARK: - Properties
+
+    // MARK: Private properties
+
+    private weak var window: UIWindow?
+
+    // MARK: - Initialization
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    // MARK: - Methods
+
+    // MARK: Coordinator protocol methods
+
+    func start() {
+        let view = ContentView()
+        let controller = UIHostingController(rootView: view)
+
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+    }
+
+}
