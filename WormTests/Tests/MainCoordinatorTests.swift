9f9ccd5038633db74aab6aa2007dcd33db9acc0f
Nikita Lazarev-Zubov <nikita.lazarev-zubov@dreambroker.fi>
2020-05-20 19:20:59 +0300

    Add tests to the main coordinator

---
+++ WormTests/Tests/MainCoordinatorTests.swift
@@ -0,0 +1,46 @@
+//
+//  MainCoordinatorTests.swift
+//  WormTests
+//
+//  Created by Nikita Lazarev-Zubov on 20.5.2020.
+//  Copyright © 2020 Nikita Lazarev-Zubov. All rights reserved.
+//
+
+@testable
+import Worm
+import XCTest
+
+final class MainCoordinatorTests: XCTestCase {
+
+    // MARK: - Methods
+
+    func testWindowRetain() {
+        var window = UIWindow()
+        weak var weakWindow = window
+
+        _ = MainCoordinator(window: window)
+        XCTAssertNotNil(weakWindow)
+
+        window = UIWindow()
+        XCTAssertNil(weakWindow)
+    }
+
+    func testWindowHasRootViewControllerAfterStart() {
+        let window = UIWindow()
+        let coordinator = MainCoordinator(window: window)
+        XCTAssertNil(window.rootViewController)
+
+        coordinator.start()
+        XCTAssertNotNil(window.rootViewController)
+    }
+
+    func testWindowIsKeyAfterStart() {
+        let window = UIWindow()
+        let coordinator = MainCoordinator(window: window)
+        XCTAssertFalse(window.isKeyWindow)
+
+        coordinator.start()
+        XCTAssertTrue(window.isKeyWindow)
+    }
+
+}
