378d5b07abdfdd836c8e2689dc297d8537340ebb
Nikita Lazarev-Zubov <nikita.lazarev-zubov@dreambroker.fi>
2020-05-27 21:02:39 +0300

    Complement unit tests

--- WormUITests/Tests/MainScreenTests.swift
+++ WormUITests/Tests/MainScreenTests.swift
@@ -26,6 +26,35 @@ final class MainScreenTests: XCTestCase {
         XCTAssertTrue(searchBar.isHittable)
     }
 
+    func testKeyboardActivation() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let searchBar = app.searchFields["searchBar"]
+        wait(forElement: searchBar)
+        searchBar.tap()
+
+        let updated = NSPredicate(format: "count > 0")
+        expectation(for: updated, evaluatedWith: app.keyboards)
+        waitForExpectations(timeout: 5.0)
+    }
+
+    func testKeyboardDeactivation() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let searchBar = app.searchFields["searchBar"]
+        wait(forElement: searchBar)
+        searchBar.tap()
+
+        let tables = app.tables.element
+        tables.tap()
+
+        let updated = NSPredicate(format: "count == 0")
+        expectation(for: updated, evaluatedWith: app.keyboards)
+        waitForExpectations(timeout: 1.0)
+    }
+
     func testResultsInitiallyEmpty() {
         let app = XCUIApplication()
         app.launch()
@@ -44,27 +73,38 @@ final class MainScreenTests: XCTestCase {
 
         // Waits for update.
         let updated = NSPredicate(format: "count > 0")
-        expectation(for: updated, evaluatedWith: app.tables.staticTexts)
+        expectation(for: updated, evaluatedWith: app.tables.cells)
         waitForExpectations(timeout: 1.0)
 
-        let expectedTexts = ["J.R.R. Tolkien", "The Lord of the Rings",
-                             "Michael Bond", "Paddington Pop-Up London",
-                             "J.K. Rowling", "Harry Potter and the Sorcecer's Stone",
-                             "George R.R. Martin", "A Game of Thrones",
-                             "Frank Herbert", "Dune I",
-                             "Mikhail Bulgakov", "The Master and Margarita",
-                             "Alan Moore", "Watchmen",
-                             "Steve McConnell", "Code Complete",
-                             "Jane Austen", "Pride and Prejudice",
-                             "Martin Fowler", "Refactoring: Improving the Design of Existing Code",
-                             "Stephen King", "The Shining",
-                             "Hannah Arendt", "Eichmann in Jerusalem: A Report on the Banality of Evil",
-                             "Fyodor Dostoyevsky", "The Idiot",
-                             "Ken Kesey", "Sometimes a Great Notion",
-                             "Haruki Murakami", "The Wind-Up Bird Chronicle"]
+        var visibleCellsCount = 0
+        let cells = app.tables.cells
+        for cellIndex in 0..<cells.count {
+            if cells.element(boundBy: cellIndex).isHittable {
+                visibleCellsCount += 1
+            } else {
+                break
+            }
+        }
+
+        let expectedTexts = ["J.R.R. Tolkien – The Lord of the Rings",
+                             "Michael Bond – Paddington Pop-Up London",
+                             "J.K. Rowling – Harry Potter and the Sorcecer's Stone",
+                             "George R.R. Martin – A Game of Thrones",
+                             "Frank Herbert – Dune I",
+                             "Mikhail Bulgakov – The Master and Margarita",
+                             "Alan Moore – Watchmen",
+                             "Steve McConnell – Code Complete",
+                             "Jane Austen – Pride and Prejudice",
+                             "Martin Fowler – Refactoring: Improving the Design of Existing Code",
+                             "Stephen King – The Shining",
+                             "Hannah Arendt – Eichmann in Jerusalem: A Report on the Banality of Evil",
+                             "Fyodor Dostoyevsky – The Idiot",
+                             "Ken Kesey – Sometimes a Great Notion",
+                             "Haruki Murakami – The Wind-Up Bird Chronicle"]
         // Depending on the screen size. In the specified order.
-        for textIndex in 0..<app.tables.staticTexts.allElementsBoundByAccessibilityElement.count {
-            XCTAssertTrue(app.tables.staticTexts[expectedTexts[textIndex]].exists)
+        for cellIndex in 0..<visibleCellsCount {
+            let cell = app.otherElements[expectedTexts[cellIndex]]
+            XCTAssertTrue(cell.exists)
         }
     }
     
