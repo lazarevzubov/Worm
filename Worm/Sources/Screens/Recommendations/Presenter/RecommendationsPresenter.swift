47436716607c21a7d8563daf6719110a95cb2740
Nikita Lazarev-Zubov <nikita.lazarev-zubov@dreambroker.fi>
2020-07-15 20:11:17 +0300

    Implement recommendations preview presenter

--- Worm/Sources/Screens/Recommendations/Presenter/RecommendationsPresenter.swift
+++ Worm/Sources/Screens/Recommendations/Presenter/RecommendationsPresenter.swift
@@ -10,6 +10,9 @@ import Combine
 import Foundation
 import GoodreadsService
 
+// TODO: Marking favorites from this screen.
+// TODO: Not displaying favorites.
+
 // TODO: HeaderDoc.
 protocol RecommendationsPresenter: ObservableObject {
 
@@ -86,7 +89,31 @@ final class RecommendationsPreviewPresenter: RecommendationsPresenter {
 
     // MARK: RecommendationsPresenter protocol properties
 
-    // TODO.
-    var recommendations = [BookViewModel]()
+    var recommendations = [
+        BookViewModel(authors: "J.R.R. Tolkien", favorite: true, id: "1", title: "The Lord of the Rings"),
+        BookViewModel(authors: "Michael Bond", favorite: true, id: "2", title: "Paddington Pop-Up London"),
+        BookViewModel(authors: "J.K. Rowling",
+                      favorite: true,
+                      id: "3",
+                      title: "Harry Potter and the Sorcecer's Stone"),
+        BookViewModel(authors: "George R.R. Martin", favorite: true, id: "4", title: "A Game of Thrones"),
+        BookViewModel(authors: "Frank Herbert", favorite: true, id: "5", title: "Dune I"),
+        BookViewModel(authors: "Mikhail Bulgakov", favorite: true, id: "6", title: "The Master and Margarita"),
+        BookViewModel(authors: "Alan Moore", favorite: true, id: "7", title: "Watchmen"),
+        BookViewModel(authors: "Steve McConnell", favorite: true, id: "8", title: "Code Complete"),
+        BookViewModel(authors: "Jane Austen", favorite: true, id: "9", title: "Pride and Prejudice"),
+        BookViewModel(authors: "Martin Fowler",
+                      favorite: true,
+                      id: "10",
+                      title: "Refactoring: Improving the Design of Existing Code"),
+        BookViewModel(authors: "Stephen King", favorite: true, id: "11", title: "The Shining"),
+        BookViewModel(authors: "Hannah Arendt",
+                      favorite: true,
+                      id: "12",
+                      title: "Eichmann in Jerusalem: A Report on the Banality of Evil"),
+        BookViewModel(authors: "Fyodor Dostoyevsky", favorite: true, id: "13", title: "The Idiot"),
+        BookViewModel(authors: "Ken Kesey", favorite: true, id: "14", title: "Sometimes a Great Notion"),
+        BookViewModel(authors: "Haruki Murakami", favorite: true, id: "15", title: "The Wind-Up Bird Chronicle")
+    ]
 
 }
